81e607d07903c973bcc63411967fa5a7f453d738
Madina Ibrayeva <78560743+mibrayevaa@users.noreply.github.com>
2022-04-22 15:12:02 +0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#So our assignment is to convert previous code to the matrix
+makeCacheMatrix<-function(x=matrix()){
+#first function takes arg as matr - matrix
+ firstvar<-NULL
+ set<-function(c){
+#then we take first var that equal NULL
+ x<-c
+ firstvar<<-NULL
+}
+ get<-function()x
+ persolving<-function(inverse)firstvar<<-invese
+ getsolve<-function()firstvar
+ list(set=set,get=get,persolving=persolving,getsolve=getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+ firstvar<-x$getsolve()
+ if(!is.null(firstvar)){
+ message('error')
+ return(x)
 }
+ data<-x$get()
+ firstvar<-solve(data,...)
+ x$persolving(firstvar)
+ firstvar
+}
+FirstMatr<-makeCacheMatrix(matrix(1:4,2,2))
+FirstMatr$get()
+FirstMatr$getsolve()
+
